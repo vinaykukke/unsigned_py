7c0eaf7035423414fd693f7489d9040051bd9a57
vinay kukke <kukke06@gmail.com>
2023-10-21 19:12:35 +0530

    Adding a startup script

--- tools/houdini/helper.py
+++ tools/houdini/helper.py
@@ -12,7 +12,7 @@ def check_existing(geometry_name: str):
     # Check if exists
     if hou.node(f"/obj/{geometry_name}"):
         # Display fail message
-        hou.ui.displayMessage(f"{geometry_name} already exists in the scene")
+        hou.ui.displayMessage(f"{geometry_name} already exists in the scene") # type: ignore
         node_exists = True
 
     return node_exists
