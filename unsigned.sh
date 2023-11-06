00d98995be34a0ee41cf6808d24c8e66ff9294c0
vinay kukke <kukke06@gmail.com>
2023-11-06 19:09:27 +0530

    Cleaning up the code

--- unsigned.sh
+++ unsigned.sh
@@ -1,74 +1,88 @@
-#! /bin/zsh
+#!/bin/bash
 
 # Define the correct directory path
 unsigned_dir="/Users/vinaykukke/Documents/Work/unsigned_py"
 hython="/Applications/Houdini/Houdini19.5.716/Frameworks/Houdini.framework/Versions/19.5/Resources/bin/hython"
 
-# The main unsigned CLI
-function unsigned() {
-  if [ "$#" -lt 1 ]; then
-    echo "Error: At least one argument is required."
-  fi
-
-  if [ $1 = "init" ]; 
-  then
-    __init
-  elif [ $1 = "install" ]; 
-  then
-    if [ $2 = "hpip" ]; then
-      __check $2
-      pip_exists=$?
-
-      if [ $pip_exists -eq 1 ]; then
-        echo "PIP already exists!"
-        echo "You can use it with the following command: hpip install numpy"
-      else
-        __pip
-      fi
-    else
-      echo "'$2' is not a known command" >&2
-    fi
-  else
-    # Show a helpful error
-    echo "'$1' is not a known command" >&2
-  fi
+# Function to display usage information
+show_help() {
+  echo "Usage: $0 [command] [options]"
+  echo "Commands:"
+  echo "  init        Initialize the directory"
+  echo "  install     Install packages (e.g., 'install hpip')"
+  echo ""
+  echo "HPIP Commands:"
+  echo "  NOTE:       You can use 'hpip' exactly like you would use regular pip"
+  echo "  install     Install packages inside the houdini environment (e.g., 'hpip install numpy')"
+  echo ""
+  echo "Options:"
+  echo "  -h, --help  Show this help message"
 }
 
-# hpip is the houdini python package manager
-function hpip() {
-  $hython -m pip $@
+# The main unsigned CLI
+unsigned() {
+  if [ "$#" -lt 1 ]; then
+    echo "Error: At least one argument is required."
+    show_help
+    exit 1
+  fi
+
+  case "$1" in
+    -h|--help)
+      show_help
+      ;;
+    init)
+      __init
+      ;;
+    install)
+      if [ -n "$2" ]; then
+        case "$2" in
+          hpip)
+            __install_hpip
+            ;;
+          *)
+            echo "'$2' is not a known subcommand" >&2
+            show_help
+            exit 1
+            ;;
+        esac
+      else
+        echo "Subcommand 'install' requires a package name (e.g., 'install hpip')" >&2
+        show_help
+        exit 1
+      fi
+      ;;
+    *)
+      echo "'$1' is not a known command" >&2
+      show_help
+      exit 1
+      ;;
+  esac
+}
+
+# hpip is the Houdini Python package manager
+function __install_hpip() {
+  # Check if the user is in the correct directory
+  if [ "$PWD" != "$unsigned_dir" ]; then
+    echo "You are not in the correct directory. Please move to: $unsigned_dir and then install."
+    exit 1
+  else
+    echo "Installing PIP in the Houdini environment..."
+    curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    $hython ./get-pip.py
+    echo "All set! You can now use PIP in Houdini by calling: hpip"
+  fi
 }
 
 function __init() {
   # Move to the parent dir
-  cd ../
+  cd ..
   echo 'Initializing your directory!'
-  # Make a unsigned_project directory
+  # Make an unsigned_projects directory
   mkdir unsigned_projects
   # Call the PM script to initiate
   zsh ./unsigned_py/project_manager.sh
 }
 
-function __pip() {
-   # Check if the user is in the correct directory
-  if [ "$PWD" != "$unsigned_dir" ]; then
-    echo "You are not in the correct directory. Please move to: $unsigned_dir and then install!"
-    exit 1
-  else
-    echo "Installing PIP in the houdini environament..."
-    curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    $hython ./get-pip.py
-    echo "All set! You can now use PIP in houdini by calling: hpip"
-  fi
-}
-
-function __check() {
-  # Check if the "get-pip.py" file exists in the current directory
-  if [ -e "get-pip.py" ]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
+# Call the main unsigned function with command line arguments
 "$@"
