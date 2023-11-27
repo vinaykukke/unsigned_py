261c1f60c2cfeda001c683653540c1850e95ab49
vinay kukke <kukke06@gmail.com>
2023-11-27 16:05:22 +0530

    adding startup script to organize the layout of nodes

--- tools/houdini/settings/scripts/123.py
+++ tools/houdini/settings/scripts/123.py
@@ -16,18 +16,65 @@ def create_camera():
     node.setDisplayFlag(False)
 
 # Create Mantra - PBR driver
-# def mantra_driver():
-#   node = hou.node("/out")
-#   out = node.createNode("ifd")
-#   out.setParms({"vm_renderengine": "pbrraytrace", "override_camerares": True, "camera": "/obj/cam_1080"})
+def mantra_driver():
+  node = hou.node("/out")
+  out = node.createNode("ifd")
+  out.setParms({"vm_renderengine": "pbrraytrace", "override_camerares": True, "camera": "/obj/cam_1080"})
+
+def create_geo():
+    """
+    Creates the basic geometry nodes required
+    """
+    geo = hou.node("/obj").createNode("geo", "GEO")
+    # geo.moveToGoodPosition()
+    geo.setDisplayFlag(True)
+
+
+def organize_layout():
+    # Get the OBJ level
+    obj_level = hou.node("/obj")
+
+    # Get all nodes at the OBJ level
+    all_nodes = obj_level.children()
+
+    if not all_nodes:
+        print("No nodes found at the OBJ level.")
+        return
+
+    # Get the number of nodes to arrange
+    num_nodes = len(all_nodes)
+
+    # Specify the number of columns and rows (both set to n)
+    # Square root to get a square layout, you can adjust this based on your preference
+    n = int(num_nodes**0.5)
+    num_columns = n
+    # num_rows = n
+
+    # Set the initial position
+    x_position = 0
+    y_position = 0
+
+    # Iterate through all nodes at the OBJ level and set their positions
+    for i, node in enumerate(all_nodes):
+        node.setPosition(hou.Vector2(x_position, y_position))
+
+        # Update the position for the next node
+        x_position += 4  # Move by 4 units for the next column
+
+        # If we reach the end of a row, reset the x-position and move to the next row
+        if (i + 1) % num_columns == 0:
+            x_position = 0
+            y_position -= 4  # Move down by 4 units for the next row
+
 
 def main():
     """
     Main function
     """
     create_camera()
-#   mantra_driver()
-
+    create_geo()
+    mantra_driver()
+    organize_layout()
 
 if __name__ == '__main__':
     main()
