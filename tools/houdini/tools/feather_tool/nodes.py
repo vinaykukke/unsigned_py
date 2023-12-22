b9d0a82e55c4262986f6dc38ec331c58c5c2cefb
vinay kukke <kukke06@gmail.com>
2023-12-22 12:39:26 +0530

    Wrapping up the automation of adding painted attributes

--- tools/houdini/tools/feather_tool/nodes.py
+++ tools/houdini/tools/feather_tool/nodes.py
@@ -1,5 +1,6 @@
-from enum import Enum
 import hou
+import sys
+from enum import Enum
 
 class Paths(Enum):
     ASSET = hou.node(".").path()
@@ -9,9 +10,23 @@ class Parameters(Enum):
     CURVES = hou.parm(f"{Paths.ASSET.value}/merge_curves").eval()
     ANIMATED_SKIN = hou.parm(f"{Paths.ASSET.value}/merge_animated_skin").eval()
 
+def check():
+    eagle = hou.node(Parameters.SKIN.value).parent()
+
+    for child in eagle.children():
+        if (child.name() == "OUT_eagle"):
+            hou.ui.displayMessage("All the attributes have already been created!!") #type: ignore
+            sys.exit()
+
 def create():
+    # Check if all the attributes have been created and only then proceed
+    check()
+
+    # Create attributes
     skin_path = hou.node(Parameters.SKIN.value).parent().path()
-    # painted_attributes = ["painted_scale"]
+    # Create a null node
+    null_node = hou.node(skin_path).createNode("null", "OUT_eagle")
+    # Attributes
     painted_attributes = ["painted_density", "painted_scale", "random_painted_scale", "painted_ribbon", "painted_tilt", "painted_rotation", "painted_concave"]
 
     for i, attribute in enumerate(painted_attributes):
@@ -24,15 +39,25 @@ def create():
         attribute_create.parm("value1v1").set(1)
         attribute_paint.parm("attribname1").set(attribute)
 
+        # Connect the creat to the paint node
+        attribute_paint.setNextInput(attribute_create)
+
         # Set the output of prev attribut_paint to the next attribute_create
         if (i > 0):
             prev_attribute_paint = hou.node(f"{skin_path}/{painted_attributes[i-1]}")
             attribute_create.setNextInput(prev_attribute_paint)
         else:
-            tranform_node = hou.node(f"{skin_path}/transform")
+            tranform_node = hou.node(f"{skin_path}/OUT_transform")
             attribute_create.setNextInput(tranform_node)
 
         # Connect the last node to the output/null node
         if (i == len(painted_attributes) - 1):
-            null_node = hou.node(f"{skin_path}/null")
             null_node.setNextInput(attribute_paint)
+        
+    # Layout children and set render flag for null
+    hou.node(skin_path).layoutChildren()
+    null_node.setDisplayFlag(True)
+    null_node.setRenderFlag(True)
+
+    # Change the merge_skin parameter to point to the new NULL
+    hou.parm(f"{Paths.ASSET.value}/merge_skin").set(null_node.path())
