a95bae1d2fd35f15c8793f35628768d1ab52f464
vinay kukke <kukke06@gmail.com>
2023-12-30 11:29:40 +0530

    Fixing small bugs in the clustering implementation

--- tools/houdini/digital_assets/feather_tool/clusters.py
+++ tools/houdini/digital_assets/feather_tool/clusters.py
@@ -3,12 +3,19 @@ import math
 import sys
 import hou
 
+asset = hou.node(".").path()
+cluster_sizes = hou.parm(f"{asset}/cluster_core").rawValue()
+in_cluster = hou.node(f"{asset}/feather_tool/IN_CLUSTER")
+children = hou.node("./feather_tool").children()
+cluster_switch = hou.node("./feather_tool/cluster_switch")
+loop_range = (int(cluster_sizes) * 2) - 2
+
 def powers_of_2(n):
     powers = [2**i for i in range(n+1) if i > 0]
     return powers
 
 def get_sizes():
-    sizes = []
+    sizes = [0, 0]
     depth = 8
 
     for i in powers_of_2(depth):
@@ -27,27 +34,25 @@ def find_power_of_2(number):
     return int(math.log2(number))
 
 def check_clusters():
-    children = hou.node("./feather_tool").children()
-    cluster_switch = hou.node("./feather_tool/cluster_switch")
+    cluster_inputs = cluster_switch.inputs()
 
     for c in children:
         if ("delete_cluster" in c.name()):
             c.destroy()
 
-    for i in cluster_switch.inputs():
+    for _ in cluster_inputs:
         cluster_switch.setFirstInput(None)
+    
+    if (int(cluster_sizes) == 0):
+        cluster_switch.setFirstInput(in_cluster)
 
 def create():
     # Check for existing cluster and destroy them
     check_clusters()
 
     # After all the clusters are destroyed then add new ones
-    asset = hou.node(".").path()
-    cluster_sizes = hou.parm(f"{asset}/cluster_core").rawValue()
-    loop_range = (int(cluster_sizes) * 2) - 2
-    in_cluster = hou.node(f"{asset}/feather_tool/IN_CLUSTER")
-    cluster_switch = hou.node(f"{asset}/feather_tool/cluster_switch")
-    cluster_switch.setFirstInput(None)
+    if (int(cluster_sizes) != 0):
+        cluster_switch.setFirstInput(None)
 
     for i in range(loop_range):
         delete_node = hou.node(f"{asset}/feather_tool").createNode("delete", f"delete_cluster_{i}")
