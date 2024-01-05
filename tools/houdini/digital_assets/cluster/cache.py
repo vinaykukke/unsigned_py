f4c8bd452e7eb1312f451e898f93befb644e462e
vinay kukke <kukke06@gmail.com>
2024-01-05 13:29:09 +0530

    WIP - cluster HDA in the making

--- tools/houdini/digital_assets/cluster/cache.py
+++ tools/houdini/digital_assets/cluster/cache.py
@@ -14,18 +14,17 @@ def create_reader_nodes():
     destroy_readers()
 
     # Then create new nodes
-    asset_name = hou.node(asset).name()
+    # asset_name = hou.node(asset).name()
     clusters = int(hou.parm(f"{asset}/cluster_core").rawValue())
     reader_node = hou.node("/obj").createNode("geo", "cluster_reader")
     out_null = reader_node.createNode("null", "OUT_READER")
     merge_node = reader_node.createNode("merge", "reader_merge")
-    r_position = hou.node(asset).position()
-    reader_node.setPosition(hou.Vector2(r_position[0], r_position[1] - 1.2))
+    reader_node.moveToGoodPosition()
     out_null.setFirstInput(merge_node)
 
     for i in range(clusters):
         file_node = reader_node.createNode("file", f"file_cluster_{i+1}")
-        file_node.parm("file").set(f"$HIP/geo/feather_tool.{asset_name}.cluster.{i+1}.$F4.bgeo.sc")
+        file_node.parm("file").set(f"$HIP/geo/$HIPNAME.cluster.{i+1}.$F4.bgeo.sc")
         # If file is not found dont show the geometry
         file_node.parm("missingframe").set(1)
         merge_node.setInput(i+1, file_node)
@@ -37,23 +36,15 @@ def create_reader_nodes():
     out_null.setRenderFlag(True)
 
 def write_to_disk():
-    c_range_min = hou.parm(f"{asset}/c_rangemin").eval()
-    c_range_max = hou.parm(f"{asset}/c_rangemax").eval()
-    frame_range = hou.parmTuple(f"{asset}/f")
-    t_range = hou.parm(f"{asset}/trange")
     output_node = hou.parm(f"{asset}/sopoutput")
     output_path = output_node.rawValue()
     clusters = int(hou.parm(f"{asset}/cluster_core").rawValue())
     select_cluster = hou.parm(f"{asset}/select_cluster")
-    save = hou.parm(f"{asset}/execute")
-
-    # Set all the frame to be cached
-    frame_range.set((c_range_min, c_range_max, 1))
-    t_range.set(1)
+    save = hou.parm(f"{asset}/cluster_cache/execute")
 
     for i in range(clusters):
         split_output_path = output_path.split(".")
-        split_output_path.insert(2, f"cluster.{i+1}")
+        split_output_path.insert(2, str(i+1))
         final_path = ".".join(split_output_path)
         output_node.set(final_path)
 
---
+++ tools/houdini/digital_assets/cluster/cluster_nodes.py
@@ -0,0 +1,83 @@
+import hou
+from enum import Enum
+
+class Paths(Enum):
+    ASSET = hou.node(".").path()
+
+def powers_of_2(n):
+    powers = [2**i for i in range(n+1) if i > 0]
+    return powers
+
+def get_sizes():
+    sizes = [0, 0]
+    depth = 8
+
+    for i in powers_of_2(depth):
+        sizes.append(i)
+        sizes.append(i)
+
+    return sizes
+
+def check_clusters():
+    cluster_sizes = int(hou.parm(f"{Paths.ASSET.value}/cluster_core").rawValue())
+    in_cluster = hou.node(f"{Paths.ASSET.value}/IN_CLUSTER")
+    children = hou.node(f"{Paths.ASSET.value}").children()
+    cluster_switch = hou.node(f"{Paths.ASSET.value}/cluster_switch")
+    cluster_inputs = cluster_switch.inputs()
+
+    for c in children:
+        if ("delete_cluster" in c.name()):
+            c.destroy()
+
+    for _ in cluster_inputs:
+        cluster_switch.setFirstInput(None)
+    
+    if (cluster_sizes == 0):
+        cluster_switch.setFirstInput(in_cluster)
+
+def create():
+    cluster_sizes = int(hou.parm(f"{Paths.ASSET.value}/cluster_core").rawValue())
+    in_cluster = hou.node(f"{Paths.ASSET.value}/IN_CLUSTER")
+    cluster_switch = hou.node(f"{Paths.ASSET.value}/cluster_switch")
+    loop_range = (cluster_sizes * 2) - 2
+
+    # Check for existing cluster and destroy them
+    check_clusters()
+
+    # After all the clusters are destroyed then add new ones
+    if (cluster_sizes != 0):
+        cluster_switch.setFirstInput(None)
+
+    for i in range(loop_range):
+        delete_node = hou.node(f"{Paths.ASSET.value}").createNode("delete", f"delete_cluster_{i}")
+        delete_node.parm("groupop").set(2)
+
+        if (i%2 != 0):
+            # delete_node.parm("rangestart").set(1)
+            delete_node.parm("filter").setExpression("@root_pt")
+
+        if (i < 2):
+            delete_node.setNextInput(in_cluster)
+        else:
+            parent_node = hou.node(f"{Paths.ASSET.value}/delete_cluster_{int(i/2) - 1}")
+            delete_node.setNextInput(parent_node)
+        
+        if (i >= (loop_range/2) - 1):
+            cluster_switch.setNextInput(delete_node)
+        
+        delete_node.moveToGoodPosition()
+
+def select():
+    cluster_sizes = int(hou.parm(f"{Paths.ASSET.value}/cluster_core").rawValue())
+    select_cluster = hou.parm(f"{Paths.ASSET.value}/select_cluster")
+    list = []
+
+    if (cluster_sizes == 0):
+        list = [0, 0]
+        select_cluster.set(0)
+    else:
+        for c in range(cluster_sizes):
+            list.append(c+1)
+            list.append(c+1)
+
+    return list
