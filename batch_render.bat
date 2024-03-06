031af8dc6de672c4a8045777a26c10a2a397db6e
vinay kukke <kukke06@gmail.com>
2024-03-06 16:08:04 +0530

    Removing unwanted files

--- batch_render.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-"C:\Program Files\Blender Foundation\Blender 3.6\blender.exe" -b "F:\Unsigned_Projects\VV_Promotion\Blender\Animation\Shot_01.blend" -a
-
-"C:\Program Files\Blender Foundation\Blender 3.6\blender.exe" -b "F:\Unsigned_Projects\VV_Promotion\Blender\Animation\Shot_02.blend" -a
-
-"C:\Program Files\Blender Foundation\Blender 3.6\blender.exe" -b "F:\Unsigned_Projects\VV_Promotion\Blender\Animation\Shot_03.blend" -a
--- batch_render_k_cycles.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-"F:\K-Cycles_2023_3.61_stable_v20230721_win_rtx\K-Cycles_2023_3.61_stable_v20230721_win_rtx\blender.exe" -b "F:\Unsigned_Projects\VV_Promotion\Blender\Animation\Shot_01.blend" -a
-
-"F:\K-Cycles_2023_3.61_stable_v20230721_win_rtx\K-Cycles_2023_3.61_stable_v20230721_win_rtx\blender.exe" -b "F:\Unsigned_Projects\VV_Promotion\Blender\Animation\Shot_02.blend" -a
-
-"F:\K-Cycles_2023_3.61_stable_v20230721_win_rtx\K-Cycles_2023_3.61_stable_v20230721_win_rtx\blender.exe" -b "F:\Unsigned_Projects\VV_Promotion\Blender\Animation\Shot_03.blend" -a
-
